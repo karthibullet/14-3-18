aaa521cf09a96404dbdbdf0fba2fd2835593a16d
karthibullet <35765123+karthibullet@users.noreply.github.com>
2018-03-14 15:44:22 +0530

    109.c

---
+++ 109.c
@@ -0,0 +1,20 @@
+
+#include<stdio.h>
+
+int main(void)
+
+{
+int i,a[100],min;
+for(i=0;i<10;i++)
+{
+	scanf("%d",&a[i]);
+	min=a[0];
+}
+for(i=0;i<10;i++)
+{
+	if(a[i]<min)
+	min=a[i];
+}
+printf("%d",min);
+	return 0;
+}
