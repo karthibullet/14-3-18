eef365b17167c5c3fbe294c3b644b2df5e8d1c96
karthibullet <35765123+karthibullet@users.noreply.github.com>
2018-03-14 16:06:17 +0530

    103.c

---
+++ 103.c
@@ -0,0 +1,20 @@
+
+#include<stdio.h>
+int main()
+{
+int n,a[55],k,count=0;
+for(i=1;i<=n;i++)
+{
+scanf("%d",&a[i]);
+}
+scanf("%d",&k0;
+for(i=1;i<=n;i++)
+{
+if(a[i]==n)
+{
+cout++;
+}
+}
+printf("%d",cout);
+return 0;
+}
